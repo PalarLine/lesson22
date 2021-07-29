9f5ebe6b5d649e3737ee43ffefef1179cd042840
palarline <kennelpalarline@gmail.com>
2021-07-29 17:43:17 +0300

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,16 @@
-
-
-//  // Валидация значений вводимых в теги Input
-
-//  document.addEventListener('DOMContentLoaded' , (event) => {
-
-//     // Валидация Input введите вес в кг
-
-//     let x  = document.getElementById('weightInput')
-//     x.addEventListener('input', (e) => {
-    
-//      let regexpWeight = /^\d{1,3}((\.|\,)\d{1})*$/; // регулярное выражения для ввода массы тела в  кг 
-        
-//     let str = e.target.value;
-
-//     let result1 = regexpWeight.test(str);
-    
-    
-//     console.log(result1);
-//     // if (!result1) {
-//     //     console.log(`ok`)
-//     //     messAlert.classList.remove('d-none');
-//     //     buttonGo.classList.add('d-none');
-//     // }
-//     return result1;
-//     })
-
-//     // Валидация Input введите рост в м
-
-//     let у  = document.getElementById('heightInput')
-//     у.addEventListener('input', (e) => {
-    
-//     let regexpHeight = /^([1-2]{1}|([1-2]{1}[0-9]{2}|[0-2]{1}(\.|\,)[0-9]{1,2}))$/; // регулярное выражения для ввода роста в м и см 
-    
-//     let str = e.target.value;
-
-//     //let result1 = regexpWeight.test(str);
-//     let result2 = regexpHeight.test(str);
-    
-//     console.log(result2);
-//     // if (!result2) {
-//     //     console.log(`ok`)
-//     //     messAlert.classList.remove('d-none');
-//     //     buttonGo.classList.add('d-none');
-//     // }
-//     return result2;
-//     })
-    
-//     }) 
-    
-
+ 
 
 function bodyMassIndexCalculation() {
-    
-    // let tag = document.getElementById('description');
-    description.classList.remove('d-none');
-    
+      
     let weight = +weightInput.value;
-    let height = +heightInput.value;     
-
+    let height = +heightInput.value;    
+    
     if (height > 3) {
-        height = height / 100
-        }
+        height = height / 100; 
+        }  
 
 
-        
-
-    if ((weight > 0 && height > 0) && (weight <= 250 && height < 3.01)) {
+    if ((weight >= 5 && height >= 0.5) && (weight <= 700 && height < 3)) {
         
         let index = (Math.round((weight / (height ** 2) *10))) / 10; 
 
@@ -84,45 +29,24 @@ function bodyMassIndexCalculation() {
                             indexDescription.innerHTML = 'Ваш индекс массы тела говорит, что у вас ожирение первой степени! Вам следует повесить замок на холодильник!';
                             } else if(index > 35 && index <= 40) {
                                 indexDescription.innerHTML = 'Ваш индекс массы тела говорит, что у вас ожирение второй степени! Запишитесь на прием к диетологу. ';
-                                } else  {indexDescription.innerHTML = 'Ваш индекс массы тела говорит, что у вас ожирение        третьей  степени! Скорее всего, вы нуждаетесь в медицинской помощи.';
-                                    }
-                            }  else {
-                                    messAlert.classList.remove('d-none');
-                                    description.classList.add('d-none');
-                                    buttonGo.classList.add('d-none');
-                                    weightInput.classList.add('d-none');
-                                    heightInput.classList.add('d-none');
-
-                                    
+                                } else if(index > 40 && index < 199) 
+                                    {indexDescription.innerHTML = 'Ваш индекс массы тела говорит, что у вас ожирение        третьей  степени! Скорее всего, вы нуждаетесь в медицинской помощи.';
+                                     }                                    
     }
-}
-                                       
-
+}                                       
+// кнопка ввести новые данные
 function clearFilds() {
-     document.getElementById('weightInput').value = "";
-     document.getElementById('heightInput').value = "";
-     indexDescription.innerHTML = "";
-     indexValue.innerHTML = "";
-     document.getElementById('weightInput').focus();
-     let tag = document.getElementById('description');
-     description.classList.add('d-none');
-     messAlert.classList.add('d-none');
-     buttonGo.classList.remove('d-none');
-    weightInput.classList.remove('d-none');
-    heightInput.classList.remove('d-none');
-      
-}
-
-function closeAlert() {
-    messAlert.classList.add('d-none');
-    buttonGo.classList.remove('d-none');
-    weightInput.classList.remove('d-none');
-    heightInput.classList.remove('d-none');
     document.getElementById('weightInput').value = "";
     document.getElementById('heightInput').value = "";
+    indexDescription.innerHTML = "";
+    indexValue.innerHTML = "";
     document.getElementById('weightInput').focus();
-
+         
 }
+
+
+
+
 
 
 
